338866d6a5e8dc8b9a3607627edea2a849be3882
stewbone <ss6422@columbia.edu>
2024-01-16 19:14:40 -0500

    backpropogation implemented. code runs and has 88.53% accuracy on MNIST. additional hidden layers reduces accuracy to 55.64%, likely bugged

--- test.py
+++ test.py
@@ -5,12 +5,13 @@ import mnist
 
 class NN:
 	def __init__(self, *args):
-		self.n = 16 #number of nodes in hidden layer
 		self.l = 784 #length of data vector
+		self.n = 20 #number of nodes in hidden layer
 		self.f = 10 #length of output layer
 		self.init_high = 0.5
 		self.init_low = -0.5 #inital range of starting weights (look into changing)
-		self.hl = 1 #number of hidden layers	
+		self.hl = 3 #number of hidden layers	
+		self.learn = 0.01 #learning rate
 		
 		if len(args) > 0:
 			self.hl = args[0]
@@ -34,16 +35,70 @@ class NN:
 	def cost(self, data, label): #cost function sum of square differences
 		return sum([pow(data[i], 2) for i in range(len(data))]) - 2*data[label] + 1
 	
-	def forwardProp(self, data):
-		activations = [data]
+	def forwardProp(self, inp):
+		activations = [np.array(inp)]
+		zs = [[]]
 		for i in range(len(self.weights)):
-			data = list(map(self.act, self.weights[i] @ data + self.bias[i]))
-			activations.append(data)
-		return activations #returns list of activations
+			z = self.weights[i] @ inp + self.bias[i] 
+			zs.append(z)
+			inp = np.array(list(map(self.act, z))) #input to next layer, or act(z)
+			activations.append(inp)
+		return activations, zs #returns list of activations
 
-	def backwardProp(self, activations, label):
-		diff = activations[-1] - label	
-		return diff
+	def backwardProp(self, forward, label):
+		lambdaWeights = []
+		lambdaBias = []
+		activations, zs = forward
+		diff = activations[-1] - label #output - label
+
+		dCdX = 2 * diff #derivative of cost function
+		dXdZ = list(map(lambda n: n * (1 - n), activations[-1])) #derivative of activation function
+		delta = np.array([dCdX[n] * dXdZ[n] for n in range(len(dCdX))]) #aka loss in the output layer
+
+		##save the first set of changes (from right to left)
+		delta.shape += (1,)
+		activations[-2].shape += (1,)
+		lambdaWeights = [delta @ np.transpose(activations[-2])] + lambdaWeights
+		lambdaBias = [delta] + lambdaBias
+
+		##we need to propogate this loss through the layers
+
+		##start going backwards, so i starts at the second to last set of weights
+		for i in range(2, len(self.weights) + 1): 
+
+			##update delta
+			dCdX = np.transpose(self.weights[-i + 1]) @ delta 
+			dXdZ = list(map(lambda n: n * (1 - n), activations[-i])) #derivative of activation function
+			delta = np.array([dCdX[n] * dXdZ[n] for n in range(len(dCdX))])
+
+			activations[-i - 1].shape += (1,)
+			lambdaWeights = [delta @ np.transpose(activations[-i - 1])] + lambdaWeights
+			lambdaBias = [delta] + lambdaBias
+		return lambdaWeights, lambdaBias
+
+	def run_epoch(self, inp, label):
+		lambdaWeights, lambdaBias = self.backwardProp(self.forwardProp(inp), label)
+		self.weights = [np.add(self.weights[i], -self.learn * lambdaWeights[i]) for i in range(len(self.weights))]
+		for bias in self.bias:
+			bias.shape += (1,)
+		self.bias = [np.add(self.bias[i], -self.learn * lambdaBias[i]) for i in range(len(self.bias))]
+
+		for i in range(len(self.bias)):
+			self.bias[i].shape = self.bias[i].shape[:-1]
+
+	def run(self, data, labels):
+		assert len(data) == len(labels)
+		for i in range(len(data)):
+			self.run_epoch(data[i], self.vectorizeLabel(labels[i]))
+	
+	def test(self, data, labels):
+		assert len(data) == len(labels)
+		correct = 0
+		for i in range(len(data)):
+			activations, zs = self.forwardProp(data[i])
+			if np.argmax(activations[-1]) == labels[i]:
+				correct += 1
+		return (correct / len(labels)) * 100
 	
 	def vectorizeLabel(self, label):
 		n = np.zeros(self.f)
@@ -58,7 +113,12 @@ if __name__ == "__main__":
 	np.random.seed(10)
 
 	## load the dataset
-	data_train, label_train, data_test, data_test = mnist.load()
+	data_train, label_train, data_test, label_test = mnist.load()
+	data_train = [img / 255 for img in data_train]
+	data_test = [img / 255 for img in data_test]
 
 	nn = NN()
-	print(nn.backwardProp(nn.forwardProp([num / 255 for num in data_train[0]]), nn.vectorizeLabel(label_train[0])))
+#	nn.forwardProp([0.1,0.5,1])	
+#	nn.backwardProp(nn.forwardProp([num / 255 for num in data_train[0]]), nn.vectorizeLabel(label_train[0]))
+	nn.run(data_train, label_train)
+	print("%f%%" % nn.test(data_test, label_test))
